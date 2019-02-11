bd59ae697e76db22fc61e06079475349d7910e75
Daniel Eid <danieleid06@gmail.com>
2019-02-11 14:00:21 +0100

    1.a.2 Arborescence profonde

---
+++ src/arborescence/ArborescenceDeep.java
@@ -0,0 +1,20 @@
+package arborescence;
+
+import java.io.File;
+
+public class ArborescenceDeep {
+
+    public void listArborescence(File pathFile){
+
+
+        for (File fileElement: pathFile.listFiles()
+                ) {
+
+            if (fileElement.isDirectory())
+                this.listArborescence(fileElement);
+
+            System.out.println(fileElement);
+        }
+    }
+
+}
--- src/arborescence/Main.java
+++ src/arborescence/Main.java
@@ -1,11 +1,16 @@
 package arborescence;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args){
 
         ArborescenceSimple a= new ArborescenceSimple();
-        a.listArborescence(".");
+        //a.listArborescence(".");
+
+        ArborescenceDeep b = new ArborescenceDeep();
+        b.listArborescence(new File("."));
 
     }
 
